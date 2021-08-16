0784392c9893e3f00c8d7f01a9ff02f579682c43
Jonathan Yu <jonyu02@gmail.com>
2021-08-16 01:45:40 -0400

    commit tree c165daab1d189478fb2348b0aa2216e63b836f4c

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                name,
+                email
+            })
+        })
+        .then(function (r) {
+            return r.json()
+        })
+        .then(function (obj) {
+            document.body.innerHTML = obj.id
+        })
+        .catch(function (err) {
+            document.body.innerHTML = err.message
+        })
+}
